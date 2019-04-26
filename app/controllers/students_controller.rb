f9c659621f4174ecb2e7b0af82c806a4b879121b
Yoonmi Kim <yoonmi.kim@rutgers.edu>
2019-04-26 19:30:56 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,4 +2,10 @@ class StudentsController < ApplicationController
   def index
     @students = Student.all
   end
+  
+  def show
+  	@student = Dinosaur.find(params[:id])
+  	Render :show
+  end
+  
 end
